825015b1ec54937b9518035f71265cd730cdfc90
tmorris <tmorris@student.42.fr>
2021-12-07 20:04:41 +0100

    Add more vector.push_back tests

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -62,6 +62,69 @@ void	test_vector_push_back_twice(UnitTest *test)
 	test->assertEqual(Vec.back(), 6);
 }
 
+void	test_vector_push_back_thrice(UnitTest *test)
+{
+	// test->assertEqual()
+	ft::vector<int> Vec;
+	Vec.push_back(5);
+	test->assertEqual(Vec.size(), static_cast<unsigned int>(1));
+	Vec.push_back(6);
+	test->assertEqual(Vec.size(), static_cast<unsigned int>(2));
+	Vec.push_back(7);
+	test->assertEqual(Vec.size(), static_cast<unsigned int>(3));
+	test->assertEqual(Vec.front(), 5);
+	test->assertEqual(Vec.back(), 7);
+}
+
+void	test_vector_push_back_x4(UnitTest *test)
+{
+	ft::vector<int> Vec;
+	Vec.push_back(5);
+	Vec.push_back(6);
+	Vec.push_back(7);
+	Vec.push_back(8);
+	test->assertEqual(Vec.size(), static_cast<unsigned int>(4));
+	test->assertEqual(Vec.front(), 5);
+	test->assertEqual(Vec.back(), 8);
+}
+
+
+void	test_vector_push_back_x5(UnitTest *test)
+{
+	ft::vector<int> Vec;
+	Vec.push_back(5);
+	Vec.push_back(6);
+	Vec.push_back(7);
+	Vec.push_back(8);
+	Vec.push_back(9);
+	test->assertEqual(Vec.size(), static_cast<unsigned int>(5));
+	test->assertEqual(Vec.front(), 5);
+	test->assertEqual(Vec.back(), 9);
+}
+
+void	test_vector_push_back_x15(UnitTest *test)
+{
+	ft::vector<int> Vec;
+	Vec.push_back(5);
+	Vec.push_back(6);
+	Vec.push_back(7);
+	Vec.push_back(8);
+	Vec.push_back(9);
+	Vec.push_back(5);
+	Vec.push_back(6);
+	Vec.push_back(7);
+	Vec.push_back(8);
+	Vec.push_back(9);
+	Vec.push_back(5);
+	Vec.push_back(6);
+	Vec.push_back(7);
+	Vec.push_back(8);
+	Vec.push_back(42);
+	test->assertEqual(Vec.size(), static_cast<unsigned int>(15));
+	test->assertEqual(Vec.front(), 5);
+	test->assertEqual(Vec.back(), 42);
+}
+
 int	main(int argc, char **argv)
 {
 	#ifndef FT_REAL_VERSION//CREATE A REAL STL EXAMPLE
@@ -101,6 +164,10 @@ int	main(int argc, char **argv)
 	test.run(test_vector_void_constructor, "Test vector<int>()");
 	test.run(test_vector_push_back, "Test vector<int>.push_back()");
 	test.run(test_vector_push_back_twice, "test_vector_push_back_twice");
+	test.run(test_vector_push_back_thrice, "test_vector_push_back_thrice");
+	test.run(test_vector_push_back_x4, "test_vector_push_back_x4");
+	test.run(test_vector_push_back_x5, "test_vector_push_back_x5");
+	test.run(test_vector_push_back_x15, "test_vector_push_back_x15");
 	
 	// test.run(example2);
 	// test.run(example3);
