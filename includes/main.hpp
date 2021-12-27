7167bb27b0bbc5f6641dd306c5e88c8aa52e4a7d
tmorris <tmorris@student.42.fr>
2021-12-27 23:02:34 +0100

    Clean up main.hpp
    More clearly differentiate between real version and my version

--- includes/main.hpp
+++ includes/main.hpp
@@ -6,15 +6,15 @@
 
 # include "UnitTest.hpp"
 
-#ifndef FT_REAL_VERSION//CREATE A REAL STL EXAMPLE
-	// #include "map.hpp"
+# ifdef FT_REAL_VERSION // Create version using real STL containers
+#  include <map>
+#  include <stack>
+#  include <vector>
+   namespace ft = std;
+# else // Create using my version by default
+// #  "map.hpp"
 	// #include "stack.hpp"
-	#include "vector.hpp"
-#else
-	#include <map>
-	#include <stack>
-	#include <vector>
-	namespace ft = std;
-#endif
+#  include "vector.hpp"
+# endif
 
 #endif
