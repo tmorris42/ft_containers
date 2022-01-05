71423d4493fa9d0ba62e3e1da82efbf484532286
tmorris <tmorris@student.42.fr>
2022-01-05 13:02:18 +0100

    vector.hpp Fix reference typedefs

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -24,8 +24,8 @@ namespace ft
 
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
-			typedef	typename allocator_type::reference			& reference;
-			typedef typename allocator_type::const_reference	& const_reference;
+			typedef	typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
 
 			// LegacyRandomAccessIterator<T>			iterator;
 			// LegacyRandomAccessIterator<T> const		const_iterator;
