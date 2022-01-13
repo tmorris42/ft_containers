0a5258369c63813b330b7d586302c40118d37cea
tmorris <tmorris@student.42.fr>
2022-01-13 18:37:26 +0100

    Fix vector typedefs

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -18,20 +18,20 @@ namespace ft
 	{
 		public:
 			// Member types
-			typedef T											value_type;
-			typedef Allocator									allocator_type;
-			typedef	typename allocator_type::reference			reference;
-			typedef typename allocator_type::const_reference	const_reference;
-			typedef typename allocator_type::pointer			pointer;
-			typedef typename allocator_type::const_pointer		const_pointer;
-			// LegacyRandomAccessIterator<T>					iterator;
-			// LegacyRandomAccessIterator<T> const				const_iterator;
-			typedef pointer										iterator;		// should be custom LRAI
-			typedef const_pointer								const_iterator;
-			typedef ft::reverse_iterator<iterator>				reverse_iterator;
-			typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
-			typedef std::ptrdiff_t								difference_type;	// should be iterator_trais<iterator>::difference_type (usually equiv. ptrdiff_t)
-			typedef std::size_t									size_type;			// An unsigned integral type that can represent any non-negative value of difference_type (Usually size_t)
+			typedef T														value_type;
+			typedef Allocator												allocator_type;
+			typedef	typename allocator_type::reference						reference;
+			typedef typename allocator_type::const_reference				const_reference;
+			typedef typename allocator_type::pointer						pointer;
+			typedef typename allocator_type::const_pointer					const_pointer;
+			// LegacyRandomAccessIterator<T>								iterator;
+			// LegacyRandomAccessIterator<T> const							const_iterator;
+			typedef pointer													iterator;		// should be custom LRAI
+			typedef const_pointer											const_iterator;
+			typedef ft::reverse_iterator<iterator>							reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>					const_reverse_iterator;
+			typedef typename ft::iterator_traits<iterator>::difference_type difference_type;	// should be iterator_trais<iterator>::difference_type (usually equiv. ptrdiff_t)
+			typedef typename allocator_type::size_type						size_type;			// An unsigned integral type that can represent any non-negative value of difference_type (Usually size_t)
 			
 			// Member functions
 			// Constructors
