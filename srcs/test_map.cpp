19c0c4336406f711298dfaef26b7b99008c5952c
tmorris <tmorris@student.42.fr>
2022-02-10 15:22:41 +0100

    Add mli_relational_operators test

--- srcs/test_map.cpp
+++ srcs/test_map.cpp
@@ -791,6 +791,50 @@ int	test_map_empty_end()
 	return (0);
 }
 
+template <class T>
+static void	cmp(const T &lhs, const T &rhs)
+{
+	static int i = 0;
+
+	std::cout << "############### [" << i++ << "] ###############"  << std::endl;
+	std::cout << "eq: " << (lhs == rhs) << " | ne: " << (lhs != rhs) << std::endl;
+	std::cout << "lt: " << (lhs <  rhs) << " | le: " << (lhs <= rhs) << std::endl;
+	std::cout << "gt: " << (lhs >  rhs) << " | ge: " << (lhs >= rhs) << std::endl;
+}
+
+int		test_map_mli_relational_ope(void)
+{
+	#define T1 char
+	#define T2 int	
+	typedef FT::map<const int, char>	map_type;
+
+	map_type mp1;
+	map_type mp2;
+
+	mp1['a'] = 2; mp1['b'] = 3; mp1['c'] = 4; mp1['d'] = 5;
+	mp2['a'] = 2; mp2['b'] = 3; mp2['c'] = 4; mp2['d'] = 5;
+
+	cmp(mp1, mp1); // 0
+	cmp(mp1, mp2); // 1
+
+	mp2['e'] = 6; mp2['f'] = 7; mp2['h'] = 8; mp2['h'] = 9;
+
+	cmp(mp1, mp2); // 2
+	cmp(mp2, mp1); // 3
+
+	(++(++mp1.begin()))->second = 42;
+
+	cmp(mp1, mp2); // 4
+	cmp(mp2, mp1); // 5
+
+	swap(mp1, mp2);
+
+	cmp(mp1, mp2); // 6
+	cmp(mp2, mp1); // 7
+
+	return (0);
+}
+
 
 void add_test_map_suite(FRAMEWORK_NAMESPACE::vector<Test2> *testlist)
 {
