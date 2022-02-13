7176788454ffce8d70817af0ed224964c46b2289
tmorris <tmorris@student.42.fr>
2022-02-13 17:23:54 +0100

    Add equal to algorithm.hpp

--- includes/algorithm.hpp
+++ includes/algorithm.hpp
@@ -35,6 +35,34 @@ namespace ft
 		}
 		return (first1 == last1) && (first2 != last2);
 	}
+
+	template <class InputIt1, class InputIt2>
+	bool equal(InputIt1 first1, InputIt1 last1,
+			   InputIt2 first2)
+	{
+		while (first1 != last1)
+		{
+			if (!(first1 == *first2))
+				return (false);
+			++first1;
+			++first2;
+		}
+		return (true);
+	}
+
+	template <class InputIt1, class InputIt2, class BinaryPredicate>
+	bool equal(InputIt1 first1, InputIt1 last1,
+			   InputIt2 first2, BinaryPredicate pred)
+	{
+		while (first1 != last1)
+		{
+			if (!pred(first1, *first2))
+				return (false);
+			++first1;
+			++first2;
+		}
+		return (true);
+	}
 }
 
 #endif
