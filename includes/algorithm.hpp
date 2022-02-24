a566f1d1133861b175ee0d61674636ba9ca4f438
tmorris <tmorris@student.42.fr>
2022-02-24 16:03:51 +0100

    Fix lexicographical_compare to be more readable

--- includes/algorithm.hpp
+++ includes/algorithm.hpp
@@ -7,16 +7,16 @@ namespace ft
 	bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
 								 InputIt2 first2, InputIt2 last2)
 	{
-		while ((first1 != last1) && (first2 != last2))
+		while (first1 != last1)
 		{
+			if (first2 == last2 || *first2 < *first1)
+				return false;
 			if (*first1 < *first2)
 				return true;
-			if (*first2 < *first1)
-				return false;
 			++first1;
 			++first2;
 		}
-		return (first1 == last1) && (first2 != last2);
+		return (first2 != last2);
 	}
 
 	template <class InputIt1, class InputIt2, class Compare>
@@ -24,16 +24,16 @@ namespace ft
 								 InputIt2 first2, InputIt2 last2,
 								 Compare comp)
 	{
-		while ((first1 != last1) && (first2 != last2))
+		while (first1 != last1)
 		{
+			if (first2 == last2 || comp(*first2, *first1))
+				return false;
 			if (comp(*first1, *first2))
 				return true;
-			if (comp(*first2, *first1))
-				return false;
 			++first1;
 			++first2;
 		}
-		return (first1 == last1) && (first2 != last2);
+		return (first2 != last2);
 	}
 
 	template <class InputIt1, class InputIt2>
